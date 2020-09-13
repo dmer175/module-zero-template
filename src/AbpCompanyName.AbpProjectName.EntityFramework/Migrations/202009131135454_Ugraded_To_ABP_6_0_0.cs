2dbe2e55bc0f113ded4859a99a75bb88549a08c1
常祈 <42560244+dmer175@users.noreply.github.com>
2020-09-13 20:19:07 +0800

    图书馆管理系统，初始版本

---
+++ src/AbpCompanyName.AbpProjectName.EntityFramework/Migrations/202009131135454_Ugraded_To_ABP_6_0_0.cs
@@ -0,0 +1,45 @@
+﻿namespace AbpCompanyName.AbpProjectName.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Ugraded_To_ABP_6_0_0 : DbMigration
+    {
+        //Library Management System
+        public override void Up()
+        {
+            CreateTable(
+                   "dbo.Lms_PublishingHouse",
+                   c => new
+                   {
+                       publishingHouseId = c.Int(nullable: false, identity: true),
+                       phNo=c.String(nullable:false, name: "出版社编号"),
+                       phName = c.String(maxLength: 250),
+                       phAdress = c.String()
+                   })
+                   .PrimaryKey(t => t.publishingHouseId);
+
+            CreateTable(
+                "dbo.Lms_Books",
+                c => new
+                {
+                    bookId = c.Int(nullable: false, identity: true),
+                    bookNo = c.String(maxLength: 250),
+                    publishingHouseId = c.String(),
+                    bookTypeId = c.String(),
+                    bookName = c.String(),
+                    Author =c.String(),
+                    price=c.Double(),
+                    totalPages=c.Int(),
+                    TotalInventory=c.Int()
+                })
+                .PrimaryKey(t => t.bookId)
+                .Index(t => t.bookTypeId)
+                .Index(t => t.publishingHouseId);
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
